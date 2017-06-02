e931d5a08fe3a4eff14d850ab77fed2ebc20417c
HectorID <hectormargittay@live.com>
2017-06-02 15:39:32 -0300

    colisao otimizada funcionando

--- src/level.cpp
+++ src/level.cpp
@@ -19,7 +19,7 @@ Level::Level() : background{Sprite(DEFAULT_BACKGROUND)}, tileSet{TileSet(DEFAULT
 	}
 }
 
-Level::Level(string file,set<unique_ptr<GameObject>>* entities) : tileSet{TileSet()}, tileMap{TileMap(DEFAULT_MAP_WIDTH, DEFAULT_MAP_HEIGHT, &tileSet)} {
+Level::Level(string file,set<unique_ptr<GameObject>>* entities) : tileSet{TileSet()}, tileMap{TileMap(DEFAULT_MAP_WIDTH, DEFAULT_MAP_HEIGHT, &tileSet)} {	
 	Load(file,entities);
 }
 
@@ -36,12 +36,13 @@ void Level::Load(string file,set<unique_ptr<GameObject>>* entities) {
 	}
 	
 	string parameters;
-	
+	//Loading the background:
 	getline(in,backgroundFilename);
 	if(!backgroundFilename.empty()) background.Open(backgroundFilename);
 	in.ignore(1);
 	background.StretchToFit(WINSIZE);
 	
+	//Loading the tileset:
 	int tileWidth, tileHeight;
 	getline(in, tileSetFilename);
 	getline(in, parameters);
@@ -49,12 +50,14 @@ void Level::Load(string file,set<unique_ptr<GameObject>>* entities) {
 	tileSet.Load(tileWidth, tileHeight, tileSetFilename);
 	in.ignore(1);
 	
+	//Loading the tilemap:
 	tileMap.Load(in);
 	
+	//Loading the collision layer:
 	int mapWidth = tileMap.GetWidth();
 	int mapHeight = tileMap.GetHeight();
 	collisionLayer.clear();
-	collisionLayer.reserve(mapWidth*mapHeight);
+	collisionLayer.resize(mapWidth*mapHeight);
 
 	int t,g;
 	int grouped[mapWidth][mapHeight];
@@ -62,19 +65,23 @@ void Level::Load(string file,set<unique_ptr<GameObject>>* entities) {
 		FOR(x,mapWidth){
 			in >> t;
 			in.ignore(1);
-			in >> g;
-			in.ignore(1);
 			collisionLayer[(y*mapWidth)+x] = t-1;
-			grouped[x][y]=g;
+			if(t == EMPTY_TILE)
+				grouped[x][y] = 0;
+			else {
+				in >> g;
+				in.ignore(1);
+				grouped[x][y] = g;
+			}
 		}
 	}
-	//in.ignore(1);
 	
+	//Setting the collision boxes:
 	if(entities!=nullptr){
 		map<int,pair<Rect,int>> mp;
 		FOR(y,mapHeight){
 			FOR(x,mapWidth){
-				t = collisionLayer[x+(y*mapWidth)]+1;
+				t = collisionLayer[(y*mapWidth)+x]+1;
 				g = grouped[x][y];
 				if(t){
 					if(!mp.count(g)){
@@ -119,32 +126,32 @@ void Level::Save(string file,vector<pair<ii,ii>> grouped) {
 		exit(EXIT_FAILURE);
 	}
 	
+	//Saving the background:
 	out<<backgroundFilename<<endl<<endl;
 	
+	//Saving the tileset:
 	out<<tileSetFilename<<endl;
 	out<<tileSet.GetWidth()<<","<<tileSet.GetHeight()<<endl<<endl;
 	
+	//Saving the tilemap:
 	tileMap.Save(out);
 	
+	//Saving the collision layer:
 	int mapWidth = tileMap.GetWidth();
 	int mapHeight = tileMap.GetHeight();
-	
-	//Collision
 	int id=1;
 	map<ii,int> ids;
 	FOR(y,mapHeight){
 		FOR(x,mapWidth){
 			char s[200];
-			if(collisionLayer[(y*mapWidth)+x]==-1)sprintf(s,"0-000, ");
+			if(collisionLayer[(y*mapWidth)+x]==EMPTY_TILE)sprintf(s,"00-000, ");
 			else{
-				auto &group = grouped[x+mapWidth*y];
+				auto &group = grouped[(mapWidth*y)+x];
 				if(group.first.first==x && group.first.second==y)ids[group.first]=id++;
-				sprintf(s,"%d-%03d, ",collisionLayer[(y*mapWidth)+x]+1,ids[group.first]);
+				sprintf(s,"%02d-%03d, ",collisionLayer[(y*mapWidth)+x]+1,ids[group.first]);
 			}
 			string str(s);
 			out << str;
-
-			// out<<collisionLayer[(y*mapWidth)+x]+1 << "-" << grouped[x][y] <<",\t";
 		}
 		out << endl;
 	}
